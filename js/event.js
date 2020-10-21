a9f81340d3f274620b9159d4f1655e6724a8c570
kadas36 <oduorthomas14@gmail.com>
2020-10-21 21:42:45 +0300

    Add categories

--- js/event.js
+++ js/event.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    alert("hjsdhsk")
     let icons = ['.icon1', '.icon2', '.icon3'];
     icons.forEach(function(icon){
         $(icon + ' .actions').hide();
@@ -7,7 +6,29 @@ $(document).ready(function(){
             $(icon + ' .actions').fadeToggle(1000);
         })
     })
+    
+    
+    
+    // $('#children').hide();
+    // $('.icon1 .actions .redirect').click(function(){
+    //     $('#children').show();
+    //     $('#girls').hide();
+    //     $('#general').hide();
 
+    // });
 
+    // $('#girls').hide();
+    // $('.icon2 .actions .redirect').click(function(){
+    //     $('#girls').show();
+    //     $('#general').hide();
+    //     $('#children').hide();
+    // });
+
+    // $('#general').hide();
+    // $('.icon2 .actions .redirect').click(function(){
+    //     $('#general').show();
+    //     $('#children').hide();
+    //     $('#girls').hide();
+    // });
 
 });   
